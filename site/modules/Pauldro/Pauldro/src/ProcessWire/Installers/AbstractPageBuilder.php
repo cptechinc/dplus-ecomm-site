a6707eb7214c873df6cb39aa5ddb1ce55498a8b4
Paul <pauldro952@outlook.com>
2024-06-26 10:52:39 -0500

    Update AbstractPageBuilder.php

--- site/modules/Pauldro/Pauldro/src/ProcessWire/Installers/AbstractPageBuilder.php
+++ site/modules/Pauldro/Pauldro/src/ProcessWire/Installers/AbstractPageBuilder.php
@@ -32,7 +32,6 @@ abstract class AbstractPageBuilder extends AbstractStaticPwInstaller {
 		// ]
 	];
 
-
 /* =============================================================
 	Page fields
 ============================================================= */
