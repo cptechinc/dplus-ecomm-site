1ef00809aeb51a82aa5fd796b5effb0161963247
Paul <pauldro952@outlook.com>
2024-06-26 11:14:37 -0500

    Create login.php

---
+++ site/templates/login.php
@@ -0,0 +1,13 @@
+<?php
+use Controllers\Login;
+
+$routes = [
+	['GET',  '', Login::class, 'index'],
+	['POST', '', Login::class, 'process'],
+];
+$router = new Mvc\Routers\Router();
+$router->setRoutes($routes);
+$router->setRoutePrefix($page->url);
+$page->body = $router->route();
+
+include __DIR__ . "/basic-blank-page.php";
