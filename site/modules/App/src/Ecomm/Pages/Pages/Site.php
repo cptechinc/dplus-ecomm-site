996c12674672df53bc05d33d1020ec7127327c03
Paul <pauldro952@outlook.com>
2024-06-27 13:53:41 -0500

    Create Site.php

---
+++ site/modules/App/src/Ecomm/Pages/Pages/Site.php
@@ -0,0 +1,18 @@
+<?php namespace App\Ecomm\Pages\Pages;
+// App
+use App\Pw\Pages\AbstractPageBuilder;
+
+/**
+ * Site
+ * Installs Site Related Pages
+ */
+class Site extends AbstractPageBuilder {
+	const PAGES = [
+		'config' => [
+			'name'     => 'config',
+			'title'    => 'Config',
+			'template' => 'site-config',
+			'parentSelector'   => '/',
+		],
+	];
+}
