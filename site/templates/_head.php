cf430e85d5b661fc88d7a25164a9e9282b6f8f8f
Paul <pauldro952@outlook.com>
2024-06-27 15:10:48 -0500

    Update _head.php

--- site/templates/_head.php
+++ site/templates/_head.php
@@ -1,7 +1,9 @@
 <?php include ('./_head-blank.php'); ?>
 
 <div class="page">
-	<?= $TWIG->render('nav/topbar.twig'); ?>
+	<?php if ($session->isLoggedInEcomm() === false) : ?>
+		<?= $TWIG->render('nav/topbar.twig'); ?>
+	<?php endif; ?>
 	
 	<header class="nav-holder make-sticky">
 		<?= $TWIG->render('nav/navbar-example.twig'); ?> 
