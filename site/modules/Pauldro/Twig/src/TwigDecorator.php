33c1bccf7c7344f3651f7ba96d3d44ffa09702d8
Paul <pauldro952@outlook.com>
2024-06-27 14:28:28 -0500

    Update TwigDecorator.php

--- site/modules/Pauldro/Twig/src/TwigDecorator.php
+++ site/modules/Pauldro/Twig/src/TwigDecorator.php
@@ -78,7 +78,7 @@ class TwigDecorator extends AbstractStaticPwClass {
 	private static function addGlobalsPw(Environment $twig) {
 		$pw = self::pw('config');
 
-		foreach (['page', 'pages', 'config', 'user', 'languages', 'sanitizer', 'session', 'input', 'browseragent'] as $variable) {
+		foreach (['page', 'pages', 'config', 'user', 'languages', 'sanitizer', 'session', 'input', 'browseragent', 'urls'] as $variable) {
 			$twig->addGlobal($variable, $pw->wire($variable));
 		};
 		$twig->addGlobal('homepage', $pw->wire('pages')->get('/'));
