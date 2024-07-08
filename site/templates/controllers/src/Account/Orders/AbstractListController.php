20e4e463b9637850b36ed5815725f2134297fee7
Paul <pauldro952@outlook.com>
2024-07-08 14:51:15 -0500

    FEAT: create, extend Orders\AbstractListController

--- site/templates/controllers/src/Account/Orders/AbstractListController.php
+++ site/templates/controllers/src/Account/Orders/AbstractListController.php
@@ -7,23 +7,50 @@ use SalesOrder as SoRecord;
 use ProcessWire\WireData;
 use ProcessWire\WireInput;
 // Dplus
+use Dplus\Abstracts\AbstractFilterData;
+use Dplus\Database\Tables\AbstractOrderTable;
 use Dplus\Database\Tables\SalesOrder as SoTable;
 // Controllers
 use Controllers\Abstracts\AbstractController;
 use Controllers\Account as AccountController;
 
 /**
- * Orders
- * Handles the Orders List page
+ * AbstractListController
+ * Template for handling the Order Lists pages
  */
-class Orders extends AbstractListController {
+abstract class AbstractListController extends AbstractController {
 	const SESSION_NS = 'sales-orders';
-	const TITLE         = 'Your Open Orders';
+	const REQUIRE_LOGIN = true;
+	const TEMPLATE      = 'account';
+	const TITLE         = 'Your Orders';
 	const PAGE_NAME     = 'orders';
+	const RESULTS_PERPAGE = 25;
 
 /* =============================================================
 	1. Indexes
 ============================================================= */
+	/**
+	 * Process HTTP GET Request
+	 * @param  WireData $data
+	 * @return string|bool
+	 */
+	public static function index(WireData $data) {
+		if (static::init() === false) {
+			return false;
+		}
+		self::pw('page')->title = static::TITLE;
+		return static::list($data);
+	}
+
+	/**
+	 * Handle Display of List Page
+	 * @param  WireData $data
+	 * @return bool
+	 */
+	protected static function list(WireData $data) {
+		$results = static::fetchListPaged($data);
+		return static::displayList($data, $results);
+	}
 
 /* =============================================================
 	3. Data Fetching / Requests / Retrieval
@@ -31,52 +58,48 @@ class Orders extends AbstractListController {
 	/**
 	 * Return Filter Data
 	 * @param  WireData $data
-	 * @return SoTable\FilterData
+	 * @return AbstractFilterData
 	 */
-	protected static function createFilterData(WireData $data) {
-		/** @var WireInput */
-		$input = self::pw('input');
-		$filter = SoTable\FilterData::fromWireInputData($input->get);
-		$filter->pagenbr = $input->pageNum();
-		$filter->limit   = static::RESULTS_PERPAGE;
-		return $filter;
-	}
-	
+	abstract protected static function createFilterData(WireData $data);
+
 	/**
 	 * Return List of Sales Orders
 	 * @param WireData $data
 	 * @return PropelModelPager[SoRecord]
 	 */
 	protected static function fetchListPaged(WireData $data) {
-		/** @var WireInput */
-		$input = self::pw('input');
-		$filter = SoTable\FilterData::fromWireInputData($input->get);
-		$filter->pagenbr = $input->pageNum();
-		$filter->limit   = static::RESULTS_PERPAGE;
-		$table = SoTable::instance();
-		return $table->findPaginatedByFilterData($filter);
+		$filter = static::createFilterData($data);
+		$TABLE  = static::getOrdersTable();
+		return $TABLE->findPaginatedByFilterData($filter);
 	}
 
 /* =============================================================
 	4. URLs
 ============================================================= */
-
+	public static function url() {
+		return AccountController::url() . static::PAGE_NAME . '/';
+	}
+	
 /* =============================================================
 	5. Displays
 ============================================================= */
+	protected static function displayList(WireData $data, PropelModelPager $results) {
+		return static::renderList($data, $results);
+	}
 
 /* =============================================================
 	6. HTML Rendering
 ============================================================= */
+	protected static function renderList(WireData $data, PropelModelPager $results) {
+		return self::getTwig()->render('account/orders/' . static::PAGE_NAME . '/list/page.twig', ['results' => $results]);
+	}
 
 /* =============================================================
 	7. Class / Module Getters
 ============================================================= */
 	/**
 	 * Return Orders Table
-	 * @return SoTable
+	 * @return AbstractOrderTable
 	 */
-	protected static function getOrdersTable() {
-		return SoTable::instance();
-	}
+	abstract protected static function getOrdersTable();
 }
