fd8a1a4e8e532d4ed3da72b4950a60981bcc6145
Paul <pauldro952@outlook.com>
2024-07-31 15:43:21 -0500

    Update Site.php

--- site/modules/App/src/Configs/Configs/Site.php
+++ site/modules/App/src/Configs/Configs/Site.php
@@ -6,10 +6,16 @@ use ProcessWire\WireArray;
  * Site
  * Container for Site Config
  * 
- * @property bool  $useTopbar  Use Topbar
+ * @property bool    $useTopbar          Use Topbar
+ * @property bool    $showProductPages   Make Product Pages Accessible?
+ * @property string  $productImportFlag  Flag / Marker to find products from ITM
+ * @property bool    $showQtyInStock     Show Numeric Qty in Stock?
  */
 class Site extends AbstractConfig {
 	public function __construct() {
 		$this->useTopbar = true;
+		$this->showProductPages = false;
+		$this->productImportFlag =  '';
+		$this->showQtyInStock    = true;
 	}
 }
