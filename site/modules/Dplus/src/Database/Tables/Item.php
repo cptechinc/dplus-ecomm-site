ddf41011e04a8416364e4218a7c08bfc96b7ca43
Paul <pauldro952@outlook.com>
2024-08-02 10:36:59 -0500

    Update Item.php

--- site/modules/Dplus/src/Database/Tables/Item.php
+++ site/modules/Dplus/src/Database/Tables/Item.php
@@ -6,14 +6,13 @@
 use ItemMasterItemQuery as Query, ItemMasterItem as Record;
 // Dplus
 use Dplus\Abstracts\AbstractQueryWrapper;
-	// use Dplus\Abstracts\AbstractFilterData;
+use Dplus\Abstracts\AbstractFilterData;
 
 /**
  * Item
  * Template for reading Records from Item table
  * 
  * @method Query query()
- * @method Query queryFilteredByFilterData(Item\FilterData $data)
  * @method Query queryFilteredByFilterData(Item\FilterData $data)
  * @static self  $instance
  */
@@ -22,6 +21,11 @@ class Item extends AbstractQueryWrapper {
 	const MODEL_KEY          = 'itemid';
 	const MODEL_TABLE        = 'inv_item_mast';
 	const DESCRIPTION        = 'Dplus Items table';
+	const COLUMNS_SEARCH = [
+		'itemid',
+		'description',
+		'description2'
+	];
 
 	protected static $instance;
 
@@ -36,7 +40,7 @@ class Item extends AbstractQueryWrapper {
 	public function queryItemid($itemID) {
 		return $this->query()->filterByItemid($itemID);
 	}
-	
+
 /* =============================================================
 	Reads
 ============================================================= */
@@ -64,6 +68,17 @@ class Item extends AbstractQueryWrapper {
 	 */
 	public function item($itemID) {
 		return $this->queryItemid($itemID)->findOne();
+	}
+
+	/**
+	 * Return Item IDs that match wildcard search
+	 * @param  AbstractFilterData $data
+	 * @return array[string]
+	 */
+	public function itemidsByWildcardSearch(AbstractFilterData $data) {
+		$q = $this->queryWildcardSearch($data->query, $data->useWildcardSearchUppercase);
+		$q->select(Record::aliasproperty('itemid'));
+		return $q->find()->toArray();
 	}
 
 /* =============================================================
