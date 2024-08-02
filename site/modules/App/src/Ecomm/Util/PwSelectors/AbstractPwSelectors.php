a34fa3d454d6ca61dd96ea7607c070a70966e2cc
Paul <pauldro952@outlook.com>
2024-08-02 11:31:47 -0500

    Update AbstractPwSelectors.php

--- site/modules/App/src/Ecomm/Util/PwSelectors/AbstractPwSelectors.php
+++ site/modules/App/src/Ecomm/Util/PwSelectors/AbstractPwSelectors.php
@@ -1,8 +1,28 @@
 <?php namespace App\Ecomm\Util\PwSelectors;
+// ProcessWire
+use ProcessWire\Sanitizer;
+// Pauldro ProcessWire
+use Pauldro\ProcessWire\AbstractStaticPwClass;
+
 /**
  * AbstractPwSelectors
- * Template class for Generating Selector STrings
+ * Template class for Generating Selector Strings
  */
-class AbstractPwSelectors {
+class AbstractPwSelectors extends AbstractStaticPwClass {
+	/**
+	 * Return Selector String In Parentheses
+	 * @param  string $selector
+	 * @return string
+	 */
+	protected static function selectorInParens($selector) {
+		return "($selector)";
+	}
 
+	/**
+	 * Return Sanitizer
+	 * @return Sanitizer
+	 */
+	protected static function pwSanitizer() {
+		return self::pw('sanitizer');
+	}
 }
