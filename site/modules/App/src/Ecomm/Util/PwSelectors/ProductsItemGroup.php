25673ccb48f19c0c6c41717c1a4755e45f1b5568
Paul <pauldro952@outlook.com>
2024-08-07 16:23:27 -0500

    Update ProductsItemGroup.php

--- site/modules/App/src/Ecomm/Util/PwSelectors/ProductsItemGroup.php
+++ site/modules/App/src/Ecomm/Util/PwSelectors/ProductsItemGroup.php
@@ -11,12 +11,25 @@ class ProductsItemGroup extends AbstractPwSelectors {
 	const SELECTOR_DPLUSID_WHITELIST = ['#'];
 
 	/**
-	 * Return selector for a  Products Item Group Page By Dplusid
+	 * Return selector for a Products Item Group Page By Dplusid
 	 * @param  string $id
 	 * @return string
 	 */
 	public static function pageByDplusid($id) {
 		$t = self::TEMPLATE;
 		return "template=$t,dplusid=$id";
+	}
+
+	/**
+	 * Return selector for Products Item Group pages search
+	 * @param  string $search
+	 * @return string
+	 */
+	public static function search($search = '') {
+		$t = self::TEMPLATE;
+		if ($t == '') {
+			return "template=$t";
+		}
+		return "template=$t,dplusid|dplusdescription|title%=$search";
 	}
 }
