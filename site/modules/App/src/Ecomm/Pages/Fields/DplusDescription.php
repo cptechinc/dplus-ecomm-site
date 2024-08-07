365bbe3b7d3c1d2e312df1d63d1fa5b4eb3e9e67
Paul <pauldro952@outlook.com>
2024-08-07 13:04:43 -0500

    PW: install dplus description

---
+++ site/modules/App/src/Ecomm/Pages/Fields/DplusDescription.php
@@ -0,0 +1,14 @@
+<?php namespace App\Ecomm\Pages\Fields;
+// Pauldro ProcessWire
+use Pauldro\ProcessWire\Installers\Fields\AbstractField;
+
+/**
+ * DplusDescription
+ * creates / updates dplusdescription field
+ */
+class DplusDescription extends AbstractField {
+	const NAME        = 'dplusdescription';
+	const LABEL       = 'Dplus Description';
+	const DESCRIPTION = 'Dplus Description';
+	const NOTES       = '';
+}
--- site/modules/App/src/Ecomm/Pages/FieldsInstaller.php
+++ site/modules/App/src/Ecomm/Pages/FieldsInstaller.php
@@ -15,6 +15,7 @@ class FieldsInstaller extends ParentInstaller {
 	const MAP = [
 		Fields\ItemDescription::NAME  => Fields\ItemDescription::class,
 		Fields\DisplayName::NAME      => Fields\DisplayName::class,
+		Fields\DplusDescription::NAME => Fields\DplusDescription::class,
 		Fields\Dplusid::NAME          => Fields\Dplusid::class,
 		Fields\Itemid::NAME           => Fields\Itemid::class,
 		Fields\SiteIcon::NAME         => Fields\SiteIcon::class,
