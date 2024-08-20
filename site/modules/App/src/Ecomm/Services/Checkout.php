d8208685d96ef1ce6c90feba773c790db1eda031
Paul <pauldro952@outlook.com>
2024-08-20 10:44:28 -0500

    FEAT: handle address updating

--- site/modules/App/src/Ecomm/Services/Checkout.php
+++ site/modules/App/src/Ecomm/Services/Checkout.php
@@ -2,7 +2,7 @@
 // Propel ORM Library
 use Propel\Runtime\Collection\ObjectCollection;
 // Dpluso Model
-use Billing as Record;
+use Billing as BillingRecord;
 // ProcessWire
 use ProcessWire\WireData;
 use ProcessWire\WireInputData;
@@ -15,6 +15,7 @@ use App\Ecomm\Database\Billing as CheckoutTable;
  * Provides Checkout Services
  * 
  * @property CheckoutTable $table
+ * @property BillingRecord $billing
  */
 class Checkout extends AbstractEcommCrudService {
 	protected static $instance;
@@ -25,6 +26,7 @@ class Checkout extends AbstractEcommCrudService {
 	public function __construct() {
 		parent::__construct();
 		$this->table = CheckoutTable::instance();
+		$this->billing = null;
 	}
 
 /* =============================================================
@@ -50,12 +52,32 @@ class Checkout extends AbstractEcommCrudService {
 	}
 
 	/**
+	 * Return Billing Record
+	 * @return BillingRecord
+	 */
+	public function billing() {
+		if (empty($this->billing) === false) {
+			return $this->billing;
+		}
+		$this->billing = $this->billingFromDb();
+		return $this->billing;
+	}
+
+	/**
+	 * Return Billing Record
+	 * @return BillingRecord
+	 */
+	public function billingFromDb() {
+		return $this->table->findOne($this->sessionID);
+	}
+
+	/**
 	 * Return Cart Data
 	 * @return Checkout\Data\Form
 	 */
 	public function form() {
 		$form = new Checkout\Data\Form();
-		$form->setFromBilling($this->table->findOne($this->sessionID));
+		$form->setFromBilling($this->billing());
 		return $form;
 	}
 
@@ -72,6 +94,9 @@ class Checkout extends AbstractEcommCrudService {
 			case 'init-checkout':
 				return $this->processInitCheckout($input);
 				break;
+			case 'update-address':
+				return $this->processUpdateAddress($input);
+				break;
 		}
 	}
 
@@ -84,6 +109,64 @@ class Checkout extends AbstractEcommCrudService {
 		$data = new WireData();
 		$this->requestInitCheckout($data);
 		return $this->table->exists($this->sessionID);
+	}
+
+	/**
+	 * Handle Update Address Request
+	 * @param  WireInputData $input
+	 * @return bool
+	 */
+	private function processUpdateAddress(WireInputData $input) {
+		$form = $this->form();
+		$form->resetTrackChanges();
+		$this->updateFormAddressFields($input, $form);
+		return $this->updateBilling($form);
+	}
+
+/* =============================================================
+	Form Updates
+============================================================= */
+	/**
+	 * Update billing / Shipping Address fields
+	 * @param  WireInputData      $input
+	 * @param  Checkout\Data\Form $form
+	 * @return bool
+	 */
+	private function updateFormAddressFields(WireInputData $input, Checkout\Data\Form $form) {
+		$sections = ['billto', 'shipto'];
+		$fields   = ['name', 'company', 'address1', 'address2', 'city', 'state', 'zip'];
+
+		foreach ($sections as $prefix) {
+			foreach ($fields as $suffix) {
+				$field = $prefix . $suffix;
+				$form->$field = $input->text($field);
+			}
+		}
+		return true;
+	}
+
+/* =============================================================
+	Billing Updates
+============================================================= */
+	/**
+	 * Update Billing Record
+	 * NOTE: saves record
+	 * @param  Checkout\Data\Form $form
+	 * @return bool
+	 */
+	private function updateBilling(Checkout\Data\Form $form) {
+		$changedFields = $form->getChanges();
+		$billing = $this->billing();
+
+		foreach ($changedFields as $fieldname) {
+			if (array_key_exists($fieldname, $form::BILLING_KEYMAP) === false) {
+				continue;
+			}
+			$field = $form::BILLING_KEYMAP[$fieldname];
+			$setField = 'set' . ucfirst($field);
+			$billing->$setField($form->$fieldname);
+		}
+		return $billing->isModified() ? boolval($billing->save()) : true;
 	}
 
 /* =============================================================
