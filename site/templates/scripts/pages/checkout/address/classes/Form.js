5980e73bc72fe6637cad01f61aee24901887c87a
Paul <pauldro952@outlook.com>
2024-08-20 11:42:48 -0500

    FEAT: add billingAddressCopyiing

--- site/templates/scripts/pages/checkout/address/classes/Form.js
+++ site/templates/scripts/pages/checkout/address/classes/Form.js
@@ -1,4 +1,13 @@
 
 class AddressForm extends SimpleForm {
-	
+	copyBillingAddressToShipping() {
+		let fields = ['name', 'company', 'address1', 'address2', 'city', 'state', 'zip'];
+		let inputs = this.inputs.fields;
+
+		console.log(inputs);
+
+		fields.forEach((suffix) => {
+			inputs['shipto' + suffix].val(inputs['billto' + suffix].val())
+		});
+	}
 }
