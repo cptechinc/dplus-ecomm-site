01da0b5b933a360b441ed0373e95f68c3ee98599
Paul <pauldro952@outlook.com>
2024-09-04 12:03:46 -0500

    FIX: extend correct field

--- site/modules/App/src/Ecomm/Pages/Fields/Body.php
+++ site/modules/App/src/Ecomm/Pages/Fields/Body.php
@@ -1,12 +1,12 @@
 <?php namespace App\Ecomm\Pages\Fields;
 // Pauldro ProcessWire
-use Pauldro\ProcessWire\Installers\Fields\AbstractFieldTextarea;
+use Pauldro\ProcessWire\Installers\Fields\AbstractFieldTextareaHtml;
 
 /**
  * Body
  * creates / updates body field
  */
-class Body extends AbstractFieldTextarea {
+class Body extends AbstractFieldTextareaHtml {
 	const NAME        = 'body';
 	const LABEL       = 'Body';
 	const DESCRIPTION = 'Body';
