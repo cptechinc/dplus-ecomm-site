1cbee55207bf615a954b7b00ecb4aebe48ab9a0f
Paul <pauldro952@outlook.com>
2024-09-04 12:03:49 -0500

    Create MapsEmbedUrl.php

---
+++ site/modules/App/src/Ecomm/Pages/Fields/MapsEmbedUrl.php
@@ -0,0 +1,14 @@
+<?php namespace App\Ecomm\Pages\Fields;
+// Pauldro ProcessWire
+use Pauldro\ProcessWire\Installers\Fields\AbstractFieldUrl;
+
+/**
+ * MapsEmbedUrl
+ * creates / updates mapsEmbedUrl field
+ */
+class MapsEmbedUrl extends AbstractFieldUrl {
+	const NAME        = 'mapsEmbedUrl';
+	const LABEL       = 'Maps Embed URL';
+	const DESCRIPTION = 'Maps Embed URL';
+	const NOTES       = '';
+}
