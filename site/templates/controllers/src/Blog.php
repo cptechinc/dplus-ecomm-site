61ad01dbf9dc989b74fd7f990c6109d86d2c170f
Paul <pauldro952@outlook.com>
2024-09-16 07:17:34 -0500

    FEAT: add template, controller, display for blog list

---
+++ site/templates/controllers/src/Blog.php
@@ -0,0 +1,81 @@
+<?php namespace Controllers;
+// ProcessWire
+use ProcessWire\WireData;
+// Controllers
+use Controllers\Abstracts\AbstractController;
+
+/**
+ * Blog
+ * Handles Blog Page
+ */
+class Blog extends AbstractController {
+	const SESSION_NS = 'blog';
+	const TEMPLATE   = 'blog';
+
+/* =============================================================
+	1. Indexes
+============================================================= */
+	public static function index(WireData $data) {
+		self::initPageHooks();
+		return self::display($data);
+	}
+
+/* =============================================================
+	2. Validations / Permissions / Initializations
+============================================================= */
+
+/* =============================================================
+	3. Data Fetching / Requests / Retrieval
+============================================================= */
+
+/* =============================================================
+	4. URLs
+============================================================= */
+	public static function url() {
+		return self::pw('pages')->get('template=blog')->url;
+	}
+
+/* =============================================================
+	5. Displays
+============================================================= */
+	private static function display(WireData $data) {
+		return self::render($data);
+	}
+
+/* =============================================================
+	6. HTML Rendering
+============================================================= */
+	private static function render(WireData $data) {
+		return self::getTwig()->render('blog/page.twig');
+	}
+
+/* =============================================================
+	7. Class / Module Getters
+============================================================= */
+
+/* =============================================================
+	8. Supplemental
+============================================================= */
+
+/* =============================================================
+	9. Hooks / Object Decorating
+============================================================= */
+	/**
+	 * Initialze Page Hooks
+	 * @param  string $tplname
+	 * @return bool
+	 */
+	public static function initPageHooks($tplname = '') {
+		// $selector = static::getPageHooksTemplateSelector();
+		// $m = self::pw('modules')->get('App');
+	}
+
+	/**
+	 * Add Hooks to Pages
+	 * @param  string $tplname
+	 * @return bool
+	 */
+	public static function initPagesHooks() {
+		// $m = self::pw('modules')->get('App');
+	}
+}
