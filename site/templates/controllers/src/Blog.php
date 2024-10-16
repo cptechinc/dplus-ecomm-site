5c1e8e3a7eae3c65a78292dfc35e32ebc3590849
Paul <pauldro952@outlook.com>
2024-10-16 14:59:18 -0500

    REFACTOR: use controller to fetch posts

--- site/templates/controllers/src/Blog.php
+++ site/templates/controllers/src/Blog.php
@@ -1,5 +1,6 @@
 <?php namespace Controllers;
 // ProcessWire
+use ProcessWire\PageArray;
 use ProcessWire\WireData;
 // Controllers
 use Controllers\Abstracts\AbstractController;
@@ -17,7 +18,9 @@ class Blog extends AbstractController {
 ============================================================= */
 	public static function index(WireData $data) {
 		self::initPageHooks();
-		return self::display($data);
+
+		$posts = self::fetchPosts($data);
+		return self::display($data, $posts);
 	}
 
 /* =============================================================
@@ -27,6 +30,18 @@ class Blog extends AbstractController {
 /* =============================================================
 	3. Data Fetching / Requests / Retrieval
 ============================================================= */
+	/**
+	 * Return Posts that have this tag
+	 * @param  WireData  $data
+	 * @return PageArray(template=blog-post)
+	 */
+	public static function fetchPosts(WireData $data) {
+		$data->pagenbr = $data->pagenbr ? $data->pagenbr : self::getPwInput()->pageNum();
+		$data->limit = self::LIMIT_ON_PAGE;
+		$data->start = self::getOffsetFromPagenbr($data->pagenbr, self::LIMIT_ON_PAGE);
+		$pages = self::getPwPages();
+		return $pages->find("template=blog-post,start=$data->start,limit=$data->limit,sort=-blog_date");
+	}
 
 /* =============================================================
 	4. URLs
@@ -38,15 +53,15 @@ class Blog extends AbstractController {
 /* =============================================================
 	5. Displays
 ============================================================= */
-	private static function display(WireData $data) {
-		return self::render($data);
+	private static function display(WireData $data, PageArray $posts) {
+		return self::render($data, $posts);
 	}
 
 /* =============================================================
 	6. HTML Rendering
 ============================================================= */
-	private static function render(WireData $data) {
-		return self::getTwig()->render('blog/page.twig');
+	private static function render(WireData $data, PageArray $posts) {
+		return self::getTwig()->render('blog/page.twig',['posts' => $posts]);
 	}
 
 /* =============================================================
