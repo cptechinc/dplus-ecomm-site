689007c0d1caa78dd6267ce2e797f0054aabcbb7
Paul <pauldro952@outlook.com>
2024-10-17 11:17:17 -0500

    FEAT: add route, controller, display for blog-author-posts

--- site/templates/blog-authors.php
+++ site/templates/blog-authors.php
@@ -1,10 +1,13 @@
 <?php
 use Controllers\Blog\BlogAuthors;
 use Controllers\Blog\BlogAuthor;
+use Controllers\Blog\BlogAuthorPosts;
 
 $routes = [
 	['GET',  '', BlogAuthors::class, 'index'],
 	['GET',  '{name}', BlogAuthor::class, 'index'],
+	['GET',  '{name}/posts', BlogAuthorPosts::class, 'index'],
+	['GET',  '{name}/posts/page{pagenbr:\d+}', BlogAuthorPosts::class, 'index'],
 ];
 $router = new Mvc\Routers\Router();
 $router->setRoutes($routes);
---
+++ site/templates/controllers/src/Blog/BlogAuthorPosts.php
@@ -0,0 +1,83 @@
+<?php namespace Controllers\Blog;
+// ProcessWire
+use ProcessWire\HookEvent;
+use ProcessWire\PageArray;
+use ProcessWire\User;
+use ProcessWire\WireData;
+use ProcessWire\WireArray;
+// Controllers
+use Controllers\Abstracts\AbstractController;
+
+
+/**
+ * BlogAuthorPosts
+ * Handles blog author's posts Page
+ */
+class BlogAuthorPosts extends BlogAuthor {
+	const SESSION_NS = 'blog-author-posts';
+	const TEMPLATE   = 'blog-authors';
+
+/* =============================================================
+	1. Indexes
+============================================================= */
+	public static function index(WireData $data) {
+		self::sanitizeParametersShort($data, ['name|pageName']);
+		
+		if ($data->name) {
+			return self::author($data);
+		}
+		self::getPwSession()->redirect(BlogAuthors::url(), $http301=false);
+	}
+
+	public static function author(WireData $data) {
+		$author = self::fetchAuthor($data);
+		
+		if (empty($author)) {
+			self::getPwSession()->redirect(BlogAuthors::url(), $http301=false);
+			return false;
+		}
+		$author->posts = self::fetchPosts($data, $author);
+
+		self::initPageHooks();
+		self::getPwPage()->tabtitle = "$author->title's Posts";
+		self::getPwPage()->title = "$author->title's Posts";
+		self::getPwPage()->tabposttitle = 'Blog';
+		return self::display($data, $author);
+	}
+
+/* =============================================================
+	2. Validations / Permissions / Initializations
+============================================================= */
+	
+/* =============================================================
+	3. Data Fetching / Requests / Retrieval
+============================================================= */
+	
+/* =============================================================
+	4. URLs
+============================================================= */
+	
+
+/* =============================================================
+	5. Displays
+============================================================= */
+	
+/* =============================================================
+	6. HTML Rendering
+============================================================= */
+	protected static function render(WireData $data, User $author) {
+		return self::getTwig()->render('blog/blog-author-posts/page.twig', ['author' => $author]);
+	}
+
+/* =============================================================
+	7. Class / Module Getters
+============================================================= */
+
+/* =============================================================
+	8. Supplemental
+============================================================= */
+
+/* =============================================================
+	9. Hooks / Object Decorating
+============================================================= */
+}
